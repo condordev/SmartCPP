43658bc485412b3237e5cf2a06e235706bd7b884
condordev <sgrad07@gmail.com>
2021-01-24 17:51:35 +0100

    note std::string goes not work with Vector

--- CustomVector/CustomVector.cpp
+++ CustomVector/CustomVector.cpp
@@ -35,15 +35,48 @@ void printVector(const Vector<Vector3>& vector) {
     }
 }
 
+
+
+
+// int a[] = { 1, 2, 3 };
+// Called with our previously defined array a will implicitly instantiate the following function:
+// size_t array_size(const int (&)[3]) { return 3 }
+// The key is that the template takes arrays by reference. (reference to an array of ints) 
+// So using a reference is necessary when we want to know the size of the passed array.
+template<typename T, size_t n>
+size_t array_size(const T(&)[n]) {   // size_t array_size(const int (&)[3]) { return 3 }
+    return n;
+}
+
+// C++ 11 version
+template<typename T, size_t S>
+constexpr auto array_size_(const T(&)[S]) -> size_t
+{
+    return S;
+}
+
 int main()
 {
+    // https://stackoverflow.com/questions/437150/can-someone-explain-this-template-code-that-gives-me-the-size-of-an-array
+    int a[] = { 1, 2, 3, 4, 5 };
+    int* ap = a; // a pointer, size is lost
+    int(&ar)[5] = a; // a reference to the array, size is not lost
+    std::cout << "Sizeof(a): " << sizeof(a) << " Number of elements in a: " << (sizeof(a) / sizeof(a[0])) << "\n";
+    auto size = array_size(a);
+    std::cout << "array_size(a) = " << size << "\n";
+    double b[] = { 1, 2, 3, 4, 5, 6, 7 };
+    std::cout << "array_size(b) = " << array_size(b) << "\n";
+
+    std::cout << "***************************************************\n";
+    std::cout << "************** Custom Array stuff *****************\n";
+    std::cout << "***************************************************\n";
     Array<std::string, 2 > data;
     static_assert(data.Size() < 10, "Size is too large");
     data[0] = "Array1";
     data[1] = "Array2";
     const auto& arrayReference = data;
-    std::cout << "data.size" << data.Size() << std::endl;
-    std::cout << "arrayReference" << arrayReference[1] << std::endl;
+    std::cout << "data.size " << data.Size() << std::endl;
+    std::cout << "arrayReference " << arrayReference[1] << std::endl;
     for (unsigned int i = 0;  i < data.Size(); i++) 
         data[i] = std::string("arrayno: ") += std::to_string(i);
     printArray(data[0]);
@@ -51,10 +84,9 @@ int main()
     // ******************  Vector *********************************
     Vector<std::string> vector;
 
-    vector.pushBack("str1");
-    vector.pushBack("str2");
-    vector.pushBack("str3");
-
+    /*
+    std::string str = "sometext";
+    vector.pushBack(str);
     printVector(vector);
 
     try
@@ -65,8 +97,11 @@ int main()
     {
         std::cout << "std::overflow_error: " << msg.what();
     }
-
+    */
+    std::cout << "***************************************************\n";
     std::cout << "******************  Vector3 stuff *****************\n";
+    std::cout << "***************************************************\n";
+
 
     Vector3 v0;
     //printVector3(v0);
@@ -74,11 +109,11 @@ int main()
     Vector3 v1( 1.0f, 2.0f, 3.0f );
     //printVector3(v1);
 
-    Vector3 v2(v1);
+    //Vector3 v2(v1);  // attempting to call a deleted function
     //printVector3(v2);
 
     Vector3 v3;
-    v3 = v2;
+    //v3 = v2;
     //printVector3(v3);
 
     std::cout << "******************  Vector c'tor *****************\n";
--- CustomVector/vector.h
+++ CustomVector/vector.h
@@ -3,6 +3,7 @@
 #include <assert.h> 
 #include <stdexcept>
 #include <memory>
+#include <algorithm>
 
 template<typename T, size_t S>
 class Array {
@@ -22,36 +23,67 @@ private:
 	T data_[S];
 };
 
+// Tthe BIG THREE stuff
+// https://stackoverflow.com/questions/3510662/vectors-within-classes-handling-copy-constructor-and-destructor-c
+// By the way, the Big Three is in the context of managing resources(copyingand destroying resources), not created them.
+// So it would be copy - constructor, copy - assignment, and destructor, not default constructor.
+// you don't need to do anything because you aren't managing any resources. 
+// You only write the Big Three when you're managing a resource, but vector is doing that. It's the one with the Big Three properly written, you just use it.
 class Vector3 {
 public:
+	// c'tor and its overloads
 	Vector3() : x(0), y(0), z(0) { 
-		//std::cout << "Vector3()" << "\n"; 
+		// IMPROVEMENT FIVE: what happens is you has to deal with resource managment in the class
+		memoryBlock = new int[5];
+		std::cout << "Vector3()" << "\n"; 
 	}
 	Vector3(float scalar) : x(scalar), y(scalar), z(scalar) { 
-		//std::cout << "Vector3(scalar)" << "\n"; 
+		memoryBlock = new int[5];
+		std::cout << "Vector3(scalar)" << "\n"; 
 	}
 	Vector3(float x, float y, float z) : x(x), y(y), z(z) { 
-		//std::cout << "Vector3(x,y,z)" << "\n"; 
+		memoryBlock = new int[5];
+		std::cout << "Vector3(x,y,z)" << "\n"; 
 	}
+	
+	/* IMPROVEMENT FIVE: not called anymore
+	// copy-constructor example: Vector3 v2(v1);
 	Vector3(const Vector3& other) : x (other.x), y(other.y), z(other.z) {
+		this->memoryBlock = new int[5];
+		std::copy(other.memoryBlock, other.memoryBlock + sizeof(int)*5, this->memoryBlock); // sizeof is compile time
 		std::cout << "Copy c`tor" << "\n";
 	}
+	*/
+	Vector3(const Vector3& other) = delete;
 
-	Vector3(const Vector3&& other) noexcept : x(other.x), y(other.y), z(other.z)
+
+	// move-constructor
+	Vector3(Vector3&& other) noexcept : x(other.x), y(other.y), z(other.z)
 	{
+		// IMPROVEMENT FIVE:
+		this->memoryBlock = other.memoryBlock; // stealing others memoryBlock
+		other.memoryBlock = nullptr;
 		std::cout << "Move c'tor" << "\n";
 	}
 
+	/* IMPROVEMENT FIVE: not called anymore
+	// copy-assigment operator example: v3 = v2;
 	Vector3& operator=(const Vector3& other) {
 		std::cout << "Copy operator=" << "\n";
+		if (this == &other) return *this;
 		x = other.x;
 		y = other.y;
 		z = other.z;
 		return *this;
 	}
+	*/
+	Vector3& operator=(const Vector3& other) = delete;
 
-	Vector3& operator=(const Vector3&& other) noexcept {
+	// move-assigment operator
+	Vector3& operator=(Vector3&& other) noexcept {
 		std::cout << "Move operator=" << "\n";
+		this->memoryBlock = other.memoryBlock; // stealing others memoryBlock
+		other.memoryBlock = nullptr;
 		x = other.x;
 		y = other.y;
 		z = other.z;
@@ -60,9 +92,12 @@ public:
 
 	~Vector3() {
 		std::cout << "Destroy" << "\n";
+		// deleting here is bad because of Vector d'tor which also deletes data_ 
+		delete[] memoryBlock;
 	}
 
 	float x, y, z;
+	int* memoryBlock = nullptr;
 };
 
 template<typename T>
@@ -78,7 +113,10 @@ public:
 		// if Vector<Vector3*> vector; is created
 		// its not the responsibility of the Vector class to free the memory you allocated 
 		// and the pointers point to so this is OK:
-		delete[] data_;
+		//delete[] data_;
+
+		clear();  // IMP FIVE this will NOT call now any destructors
+		::operator delete(data_, capacity_ * sizeof(T));
 	}
 
 	void pushBack(const T& value) {
@@ -140,20 +178,28 @@ public:
 private:
 	void reAlloc(size_t newCapacity) {
 
+		//T* newBlock = new T[newCapacity];
+		T* newBlock = (T*)::operator new(newCapacity * sizeof(T));  // IMP FIVE
+
 		if (newCapacity < size_)
 			size_ = newCapacity;
 
-		T* newBlock = new T[newCapacity];
 		for (size_t i = 0; i < size_; i++) {
 			// SECOND IMPROVEMENT cast this to be an r value reference and try to move it
 			// note that realloc from 2 to 4 elements calls Vector3() 4 times
 			// before improvement: newBlock[i] = data_[i];
 			// now there should be no copy anymore (if there is no move c'tor for the given type type it will copy)
 			newBlock[i] = std::move(data_[i]);
-
 		}
 
-		delete[] data_;
+		
+		for (size_t i = 0; i < size_; i++) {
+			data_[i].~T();
+		} // IMP FIVE
+		
+
+		//delete[] data_;
+		::operator delete(data_, capacity_ * sizeof(T));  // IMP FIVE
 		data_ = newBlock;
 		capacity_ = newCapacity;
 	}
